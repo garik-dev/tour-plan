cb231d9b6d4785266e1516a047e9b793974614ed
igorlyahov@gmail.com <igorlyahov@gmail.com>
2021-12-16 01:12:08 +1000

    fix: form modal

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
     $('.time').mask('00:00:00');
     $('.date_time').mask('00/00/0000 00:00:00');
     $('.cep').mask('00000-000');
-    $('.phone').mask('0000-0000');
+    $('.phone').mask('0(000) 000-0000');
     $('.phone_with_ddd').mask('(00) 0000-0000');
     $('.phone_us').mask('(000) 000-0000');
     $('.mixed').mask('AAA 000-S0S');
